27bdd4e6bcf838879d68621a9b7cff6a569a725e
HomamZituni <homam93@msn.com>
2025-11-21 15:45:01 -0500

    re-write of file to address errors

--- src/models/PhysicalProduct.ts
+++ src/models/PhysicalProduct.ts
@@ -1,28 +1,28 @@
-import { Product } from "./Product"
+import { Product } from "./Product";
+import { DiscountableProduct } from "./DiscountableProduct";
 
-export class PhysicalProduct extends Product {
-  constructor(
-    sku: string,
-    name: string,
-    price: number,
-    private weight: number 
-  ) {
+export class PhysicalProduct extends Product implements DiscountableProduct {
+  private _weight: number;
+
+  constructor(sku: string, name: string, price: number, weight: number) {
     super(sku, name, price);
+    this._weight = weight;
   }
 
+  get weight(): number {
+    return this._weight;
+  }
 
   get formattedWeight(): string {
-    return `${this.weight} kg`;
+    return `${this._weight} kg`;
   }
-
 
   getPriceWithTax(): number {
-    return this.price * 1.1;
+    return this.price * 1.1; // 10% tax
   }
 
-
-  displayDetails(): string {
-    return `${super.displayDetails()}, Weight: ${this.formattedWeight}`;
+  applyDiscount(percent: number): void {
+    this.price = this.price * (1 - percent / 100);
   }
 }
 
