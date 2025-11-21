ed0f6231778edf207182880492d979473a6a35b3
HomamZituni <homam93@msn.com>
2025-11-21 00:21:48 -0500

    update to digital product file

---
+++ src/models/DigitalProduct.ts
@@ -0,0 +1,27 @@
+import { Product } from "./Product";
+
+export class DigitalProduct extends Product {
+  constructor(
+    sku: string,
+    name: string,
+    price: number,
+    private fileSize: number
+  ) {
+    super(sku, name, price);
+  }
+
+
+  get formattedFileSize(): string {
+    return `${this.fileSize} MB`;
+  }
+
+  
+  getPriceWithTax(): number {
+    return this.price;
+  }
+
+
+  displayDetails(): string {
+    return `${super.displayDetails()}, File Size: ${this.formattedFileSize}`;
+  }
+}
