db7eb3efbd5769d8db895ccfb0ac358e391dc026
HomamZituni <homam93@msn.com>
2025-11-21 15:52:50 -0500

    added header, console logs, updated sorting, updated bulk discount and display

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,29 @@
-import {PhysicalProduct} from "./models/PhysicalProduct"
-import {DigitalProduct} from "./models/DigitalProduct"
-import {calculateTax} from "./utils/taxCalculator"
+import { PhysicalProduct } from "./models/PhysicalProduct";
+import { DigitalProduct } from "./models/DigitalProduct";
+import { calculateTax } from "./utils/taxCalculator";
+import { sortByName, sortByPrice } from "./utils/sortProducts";
+
 
 const laptop = new PhysicalProduct("P1", "Laptop", 1200, 2.5);
-
 const ebook = new DigitalProduct("D1", "E-Book", 30, 5);
 
 const inventory = [laptop, ebook];
 
+
+console.log("Original Inventory:");
 for (const product of inventory) {
   console.log(product.displayDetails());
   console.log("Price with tax:", calculateTax(product));
   console.log("--------------------");
 }
 
+
 laptop.applyDiscount(10);
-console.log("After 10% discount:");
+console.log("After 10% manual discount on Laptop:");
 console.log(laptop.displayDetails());
 console.log("Price with tax:", calculateTax(laptop));
 console.log("--------------------");
 
-
-import { sortByName, sortByPrice } from "./utils/sortProducts";
 
 console.log("Inventory sorted by name:");
 const sortedByName = sortByName(inventory);
@@ -37,6 +39,7 @@ for (const product of sortedByPrice) {
 }
 console.log("--------------------");
 
+
 const bulkWeightThreshold = 2; 
 const bulkDiscountPercent = 5; 
 
@@ -46,3 +49,12 @@ for (const product of inventory) {
     console.log(`Bulk discount applied to ${product.name} (${bulkDiscountPercent}% off)`);
   }
 }
+
+
+console.log("Inventory after bulk discount:");
+for (const product of inventory) {
+  console.log(product.displayDetails());
+  console.log("Price with tax:", calculateTax(product));
+  console.log("--------------------");
+}
+
