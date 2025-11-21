a21fedef32d5af33fdd38d577e040e33852e6924
HomamZituni <homam93@msn.com>
2025-11-20 23:30:31 -0500

    created product base class

---
+++ src/models/Product.ts
@@ -0,0 +1,13 @@
+export abstract class Product {
+  constructor(
+    public sku: string,
+    public name: string,
+    public price: number
+  ) {}
+
+  displayDetails(): string {
+    return `SKU: ${this.sku}, Name: ${this.name}, Price: $${this.price.toFixed(2)}`;
+  }
+
+  abstract getPriceWithTax(): number;
+}
